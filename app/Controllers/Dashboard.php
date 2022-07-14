36eb088e74b6925115cd4bf9b875338dae1ccf09
Rahmatputra <rahmatputrankri@gmail.com>
2022-07-14 09:44:36 +0700

    memnambahkan themlet adminlte3

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controllers;
+class Dashboard extends BaseController
+{
+    public function index()
+    {
+        $header['title']='Dashboard';
+        echo view('partial/header',$header);
+        echo view('partial/top_menu');
+        echo view('partial/side_menu');
+        echo view('dashboard');
+        echo view('partial/footer');
+    }
+}
