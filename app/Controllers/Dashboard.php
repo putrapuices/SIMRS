8a61d19a7682e7c68cd24aa7f790857c9588b7d9
febriramandha <febri.ramandha@gmail.com>
2022-07-14 15:00:15 +0700

    Hapus Message Popup saat load, ubah controller Dashboar, dan Edit template AdminLTE3

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -1,14 +1,15 @@
 <?php
+
 namespace App\Controllers;
+
 class Dashboard extends BaseController
 {
     public function index()
     {
-        $header['title']='Dashboard';
-        echo view('partial/header',$header);
-        echo view('partial/top_menu');
-        echo view('partial/side_menu');
-        echo view('dashboard');
-        echo view('partial/footer');
+        $data = [
+            'title' => 'SIMRS Pariaman'
+        ];
+
+        return view('dashboard/index', $data);
     }
 }
